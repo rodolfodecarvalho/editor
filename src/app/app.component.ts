7ff73bdb1db79be2b27c1c6db89d8fc1009635e6
Rodolfo Guerra <rodolfoguerraster@gmail.com>
2022-12-26 13:49:22 -0300

    add ImageResize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import Quill from 'quill';
+import ImageResize from 'quill-image-resize-module';
+
+Quill.register('modules/imageResize', ImageResize);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'editor';
+
+  modules = {};
+
+  constructor() {
+    this.modules = {
+      imageResize: {},
+      syntax: true,
+    };
+  }
 }
